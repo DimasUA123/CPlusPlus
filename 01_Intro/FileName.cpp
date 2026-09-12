61de08268de13de69b5eced133f63501379e8d08
Dimas <Dmytro2811@gmail.com>
2026-09-12 12:25:31 +0300

    4 task, ClassWork

--- 01_Intro/FileName.cpp
+++ 01_Intro/FileName.cpp
@@ -5,9 +5,30 @@ using namespace std;
 void main()
 
 {   
-    string name;
+    /*string name;
     cout << "Enter your name\n";
     cin >>  name;
-    cout << "Hi! " << name << endl;
+    cout << "Hi! " << name << endl;*/
+
+
+
+
+    //cout << "I\n\tLove\n\t\tYou\n\t\t\tC++!";
+
+    //cout << "\t...:::Resume:::...\nName\t\tIvan\nSurname\t\tIvanov\nLast name:\tIvanovich\n\nDate of birth:\t22.06.06\nCity:\t\tRivne\nAge:\t\t17\nHobby:\t\tC++";
+
+    /*const float cmInInch = 2.54;
+    int diagTV;
+    cout << "Enter diagonal of TV(inch) : ";
+    cin >> diagTV;
+    float resDiag = diagTV * cmInInch;
+    cout << "Diagonal of TV centimeter = " << resDiag;*/
+
+   /* const int days = 30;
+    int feedWeightOneDay;
+    cout << "Enter weight of feed for 1 day(grams) : ";
+    cin >> feedWeightOneDay;
+    float resWeight = (feedWeightOneDay*days)/1000.0;
+    cout << "Weight of feed for 30 days(kilograms) = " << resWeight;*/
 
 }
